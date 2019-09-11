06cd096be37f360e3a908364d25a952da5593735
Roman Vasilyev <roman@lonje.com>
2019-09-10 20:00:30 -0700

    allow internet in security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,13 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = var.cidr_blocks
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "allow_ssh"
   }
